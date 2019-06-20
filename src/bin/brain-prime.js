28f198a456b21d271c7c0fdb248d21bb478ba4fa
sergey_saramud <s.saramud@gmail.com>
2019-06-20 17:11:24 +0300

    Added new game brain-prime

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,8 @@
+#!usr/bin/node
+import { coreGame } from '..';
+import calcOperation from '../games/calcOperation';
+import findPrime from '../games/findPrime';
+
+
+console.log('Welcome to the Brain Games!\nAnswer "yes" if given number is prime. Otherwise answer "no".');
+coreGame(findPrime, calcOperation);
---
+++ src/games/findPrime.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+import { car, cdr, cons } from 'hexlet-pairs';
+import calcOperation from './calcOperation';
+
+const findPrime = (Number) => {
+  const num = Number();
+  const otherNumber = car(car(num)) * (Math.floor(Math.random() * 10) + 1);
+  let countP = otherNumber - 1;
+  // eslint-disable-next-line no-nested-ternary
+  const primeOrNo = (number, count) => ((count === 1) ? 'yes' : (number % count === 0) ? 'no' : primeOrNo(number, count - 1));
+  const pairPrime = cons(otherNumber, primeOrNo(otherNumber, countP));
+  return pairPrime;
+};
+
+findPrime(calcOperation);
+export default findPrime;
