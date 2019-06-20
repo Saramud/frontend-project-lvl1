8eb276de9bae2c7bb066561dc9d8d7a562912a13
sergey_saramud <s.saramud@gmail.com>
2019-06-20 12:38:18 +0300

    some work on files

--- src/games/calcString.js
+++ src/games/calcString.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 import { car, cdr, cons } from 'hexlet-pairs';
-import calcOperation from './calcOperation';
 
 const calcString = (Numbers) => {
   const str = Numbers();
   const stringPrimer = cdr(car(str));
+  // eslint-disable-next-line no-eval
   const stringRezult = `${eval(stringPrimer)}`;
   const pairCalc = cons(stringPrimer, stringRezult);
   return pairCalc;
--- src/games/nodFunc.js
+++ src/games/nodFunc.js
@@ -6,6 +6,7 @@ const nodFunc = (func) => {
   const firstNumber = car(cdr(func()));
   const seconNumber = cdr(cdr(func()));
   const nDel = `${firstNumber} ${seconNumber}`;
+  // eslint-disable-next-line consistent-return
   const searchDelitel = (c, b) => {
     // eslint-disable-next-line no-plusplus
     for (let i = b; i >= 1; i--) {
@@ -18,28 +19,4 @@ const nodFunc = (func) => {
   return pairnNodDel;
 };
 
-/*const nodFunc = (a) => {
-  let x = '';
-  let y = '';
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; a[i] !== ' '; i++) {
-    x += a[i];
-  }
-  // eslint-disable-next-line no-plusplus
-  for (let j = x.length + 1; j < a.length; j++) {
-    y += a[j];
-  }
-  console.log(`${x} ${y}`);
-  // eslint-disable-next-line consistent-return
-  const searchDelitel = (c, b) => {
-    // eslint-disable-next-line no-plusplus
-    for (let i = b; i >= 1; i--) {
-      if (c % i === 0 && b % i === 0) {
-        return `${i}`;
-      }
-    }
-  };
-  return searchDelitel(x, y);
-};*/
-
-export { nodFunc };
+export default nodFunc;
